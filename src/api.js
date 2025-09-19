4792d04c4193762b6830e9c86e3e75563c4b3e73
Prabhankar Tiwari <prabhankart@gmail.com>
2025-09-19 22:10:02 +0530

    update

--- src/api.js
+++ src/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-import { API_URL } from "./config";
 
-const api = axios.create({ baseURL: API_URL });
-
-export default api;
+export const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+});
